41b06d775ad5247097ae86a328e9b7493e1857c8
barsanges <barsanges@users.noreply.github.com>
2019-12-01 20:37:40 +0100

    Add a Post module

---
+++ src/Picrochole/Containers/Post.hs
@@ -0,0 +1,13 @@
+{- |
+   Module      : Picrochole.Containers.Post
+   Copyright   : Copyright (C) 2019 barsanges
+   License     : GNU GPL, version 3
+
+@Post@ is the collection of all messages exchanged between units in the game.
+-}
+
+module Picrochole.Containers.Post (
+  Post
+  ) where
+
+data Post = Post
