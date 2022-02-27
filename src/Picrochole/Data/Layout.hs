d93a107ca8f44e97feea9647c7949a8f8e4a8837
barsanges <barsanges@users.noreply.github.com>
2022-02-27 20:09:36 +0100

    Ajout de premiers éléments relatifs à la carte.

---
+++ src/Picrochole/Data/Layout.hs
@@ -0,0 +1,32 @@
+{- |
+   Module      : Picrochole.Data.Layout
+   Copyright   : Copyright (C) 2022 barsanges
+   License     : GNU GPL, version 3
+
+Structures pour la disposition du terrain.
+-}
+
+module Picrochole.Data.Layout
+  ( Ground(..)
+  , Location(..)
+  , CLocations
+  ) where
+
+import qualified Data.Vector as V
+import Picrochole.Data.Keys
+
+-- | Type de terrain associé à une case de la carte.
+data Ground = Plain
+            | Forest
+            | Road
+            | City
+            | River
+            | Bridge
+
+-- | Une case de la carte.
+data Location = Loc { ground :: Ground
+                    , height :: Int
+                    }
+
+-- | La carte, i.e. l'ensemble des cases de la carte.
+newtype CLocations = CL (V.Vector Location)
