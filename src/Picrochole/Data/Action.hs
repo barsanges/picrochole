6db7ff2d50032395a4598ed42b1d263c968f6dc7
barsanges <barsanges@users.noreply.github.com>
2022-02-28 21:32:50 +0100

    Ajout de premiers éléments pour les actions.

---
+++ src/Picrochole/Data/Action.hs
@@ -0,0 +1,14 @@
+{- |
+   Module      : Picrochole.Data.Action
+   Copyright   : Copyright (C) 2022 barsanges
+   License     : GNU GPL, version 3
+
+Actions entreprises par les unités.
+-}
+
+module Picrochole.Data.Action
+  ( Action(..)
+  ) where
+
+-- | Action entreprise par une unité.
+data Action = Still
