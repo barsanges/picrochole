454cb907e3f42b7bc9f055905d52385445b641ae
barsanges <barsanges@users.noreply.github.com>
2022-03-16 21:36:40 +0100

    Remplacement de 'MultiMap' par 'XMap' et 'XsMap'.

--- src/Picrochole/Utils/XsMap.hs
+++ src/Picrochole/Utils/XsMap.hs
@@ -1,14 +1,15 @@
 {- |
-   Module      : Picrochole.Utils.MultiMap
+   Module      : Picrochole.Utils.XsMap
    Copyright   : Copyright (C) 2022 barsanges
    License     : GNU GPL, version 3
 
 Implémentation naïve d'un dictionnaire pouvant être requêté soit par une clef
-unique, soit par un lieu.
+unique, soit par un lieu. Plusieurs valeurs peuvent être associées à un même
+lieu.
 -}
 
-module Picrochole.Utils.MultiMap
-  ( MultiMap
+module Picrochole.Utils.XsMap
+  ( XsMap
   , empty
   , fromList
   , lookupLocation
@@ -22,36 +23,36 @@ import qualified Data.Set as S
 import Picrochole.Data.Keys
 
 -- | Dictionnaire pouvant être requêté soit par une clef unique, soit par un
--- lieu.
-data MultiMap k a = MMap { content :: M.Map k a
-                         , locs :: M.Map LocationKey (S.Set k)
-                         }
+-- lieu. Plusieurs valeurs peuvent être associées à un même lieu.
+data XsMap k a = XsMap { content :: M.Map k a
+                       , locs :: M.Map LocationKey (S.Set k)
+                       }
   deriving Show
 
 -- | Un dictionnaire vide.
-empty :: MultiMap k a
-empty = MMap { content = M.empty
-             , locs = M.empty
-             }
+empty :: XsMap k a
+empty = XsMap { content = M.empty
+              , locs = M.empty
+              }
 
 -- | Construit un dictionnaire à partir d'une liste de paires clef / valeur.
-fromList :: (HasLocation a, Ord k) => [(k, a)] -> MultiMap k a
+fromList :: (HasLocation a, Ord k) => [(k, a)] -> XsMap k a
 fromList xs = foldr go empty xs
   where
     go (k, x) ms = insertKey k x ms
 
 -- | Renvoie tous les éléments associés au lieu donné.
-lookupLocation :: Ord k => LocationKey -> MultiMap k a -> [a]
+lookupLocation :: Ord k => LocationKey -> XsMap k a -> [a]
 lookupLocation here ms = case M.lookup here (locs ms) of
   Nothing -> []
   Just ls -> catMaybes (fmap (\ k -> lookupKey k ms) (S.toList ls))
 
 -- | Renvoie l'élément associé à la clef donnée.
-lookupKey :: Ord k => k -> MultiMap k a -> Maybe a
+lookupKey :: Ord k => k -> XsMap k a -> Maybe a
 lookupKey k ms = M.lookup k (content ms)
 
 -- | Insère un élément dans le dictionnaire.
-insertKey :: (HasLocation a, Ord k) => k -> a -> MultiMap k a -> MultiMap k a
+insertKey :: (HasLocation a, Ord k) => k -> a -> XsMap k a -> XsMap k a
 insertKey k x' ms = ms { content = c'
                        , locs = ls''
                        }
@@ -62,7 +63,7 @@ insertKey k x' ms = ms { content = c'
     ls' = case M.lookup k (content ms) of
       Nothing -> ls
       Just x -> case M.lookup (getLocation x) ls of
-        Nothing -> error "trying to use a malformed MultiMap"
+        Nothing -> error "trying to use a malformed XsMap"
         Just s -> if null s'
                   then M.delete (getLocation x) ls
                   else M.insert (getLocation x) s' ls
