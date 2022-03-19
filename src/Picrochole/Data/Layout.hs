8bca019f3e97ead6e93896ab70d4f94c05772cb4
barsanges <barsanges@users.noreply.github.com>
2022-03-19 21:48:15 +0100

    Ajout d'une fonction de calcul de distance dans la grille hexagonale.

--- src/Picrochole/Data/Layout.hs
+++ src/Picrochole/Data/Layout.hs
@@ -10,6 +10,7 @@ module Picrochole.Data.Layout
   ( Ground(..)
   , Location(..)
   , CLocations
+  , dist
   ) where
 
 import qualified Data.Vector as V
@@ -31,5 +32,26 @@ data Location = Loc { ground :: Ground
   deriving Show
 
 -- | La carte, i.e. l'ensemble des cases de la carte.
-newtype CLocations = CL (V.Vector Location)
+data CLocations = CL { lVec :: V.Vector Location
+                     , ncols :: Int
+                     , nrows :: Int
+                     }
   deriving Show
+
+-- | Convertit l'index en un triplet de coordonnées pour une grille hexagonale.
+-- Les hexagones sont orientés pointe en haut.
+toHexCoord :: LocationKey -> (Int, Int, Int)
+toHexCoord (LK (x, y)) = (q, r, -q-r)
+  where
+    q = y - truncate (0.5 * fromIntegral (x - (x `mod` 2)) :: Double)
+    r = x
+-- FIXME : à terme, supprimer cette étape ?
+
+-- | Calcule la distance en nombre de cases entre deux emplacements,
+-- cf. [ici](https://www.redblobgames.com/grids/hexagons).
+dist :: LocationKey -> LocationKey -> Int
+dist x y = truncate (0.5 * fromIntegral (diff) :: Double)
+  where
+    (xq, xr, xs) = toHexCoord x
+    (yq, yr, ys) = toHexCoord y
+    diff = abs (xq - yq) + abs (xr - yr) + abs (xs - ys)
