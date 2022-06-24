6a9d83bfa713ff373a48842a00bd246fa7ceb712
barsanges <barsanges@users.noreply.github.com>
2022-06-24 17:54:24 +0200

    Ajoute des ordres aux messages échangés entre les unités.

--- src/Picrochole/Data/Mail.hs
+++ src/Picrochole/Data/Mail.hs
@@ -16,6 +16,7 @@ module Picrochole.Data.Mail
   , received
   , mkHeader
   , sendReport
+  , sendOrder
   , dateLastReportSent
   , getLastReports
   ) where
@@ -33,11 +34,17 @@ import Picrochole.Data.Board
 -- | Identifiant (censément unique) d'un message.
 type MsgId = Int
 
+-- | Ensemble des messages de type `a` échangés entre les unités.
+data Register a = Register { senders :: Map UnitKey (Map UnitKey (Seq MsgId))
+                           , receivers :: Map UnitKey (Map UnitKey (Seq MsgId))
+                           , messages :: IntMap (Header, a)
+                           , last_ :: Int
+                           }
+  deriving Show
+
 -- | Les messages que les unités s'échangent.
-data Post = Post { senders :: Map UnitKey (Map UnitKey (Seq MsgId))
-                 , receivers :: Map UnitKey (Map UnitKey (Seq MsgId))
-                 , messages :: IntMap (Header, Report)
-                 , last_ :: Int
+data Post = Post { reports_ :: Register Report
+                 , orders_ :: Register Order
                  }
   deriving Show
 
@@ -51,6 +58,9 @@ data Header = Header { from_ :: UnitKey
 
 -- | Rapport d'une unité à son état-major.
 type Report = [Cell]
+
+-- | Ordre de l'état-major à un subordonné.
+type Order = CellKey
 
 -- | Indique l'expéditeur du message.
 from :: Header -> UnitKey
@@ -84,18 +94,30 @@ eta t dist = if dist < 0
   where
     dt = dist `div` 3
 
--- | Envoie un message.
-sendReport :: Header -> Report -> Post -> Post
-sendReport header report post = Post { senders = senders'
-                                     , receivers = receivers'
-                                     , messages = messages'
-                                     , last_ = idx
-                                     }
+-- | Ajoute un message à un registre.
+send :: Header -> a -> Register a -> Register a
+send header x register = Register { senders = senders'
+                                  , receivers = receivers'
+                                  , messages = messages'
+                                  , last_ = idx
+                                  }
   where
-    idx = (last_ post) + 1
-    senders' = nestedInsert (from header) (to header) idx (senders post)
-    receivers' = nestedInsert (to header) (from header) idx (receivers post)
-    messages' = IM.insert idx (header, report) (messages post)
+    idx = (last_ register) + 1
+    senders' = nestedInsert (from header) (to header) idx (senders register)
+    receivers' = nestedInsert (to header) (from header) idx (receivers register)
+    messages' = IM.insert idx (header, x) (messages register)
+
+-- | Envoie un rapport.
+sendReport :: Header -> Report -> Post -> Post
+sendReport header report post = post { reports_ = reports' }
+  where
+    reports' = send header report (reports_ post)
+
+-- | Envoie un ordre.
+sendOrder :: Header -> Order -> Post -> Post
+sendOrder header order post = post { orders_ = orders' }
+  where
+    orders' = send header order (orders_ post)
 
 -- | Insère une valeur dans un dictionnaire de dictionnaire.
 nestedInsert :: Ord k
@@ -115,10 +137,10 @@ nestedInsert k1 k2 x' xsss = M.insert k1 xs' xsss
 -- | Indique à quelle date l'unité a envoyé son dernier rapport.
 dateLastReportSent :: Post -> UnitKey -> UnitKey -> Maybe TurnCount
 dateLastReportSent post ukey hq = do
-  addressee <- M.lookup ukey (senders post)
+  addressee <- M.lookup ukey (senders (reports_ post))
   msgIds <- M.lookup hq addressee
   idx <- takeR msgIds
-  (header, _) <- IM.lookup idx (messages post)
+  (header, _) <- IM.lookup idx (messages (reports_ post))
   return (sent header)
 
 -- | Renvoie le dernier élément d'une séquence.
@@ -128,13 +150,13 @@ takeR (_ :|> x) = Just x
 
 -- | Renvoie le dernier rapport envoyé par chaque subordonné.
 getLastReports :: UnitKey -> Post -> [(Header, Report)]
-getLastReports ukey post = case M.lookup ukey (receivers post) of
+getLastReports ukey post = case M.lookup ukey (receivers (reports_ post)) of
   Nothing -> []
   Just xss -> catMaybes (fmap go (M.elems xss))
- 
+
   where
-    
+
     go :: Seq MsgId -> Maybe (Header, Report)
     go msgIds = do
       idx <- takeR msgIds
-      IM.lookup idx (messages post)
+      IM.lookup idx (messages (reports_ post))
