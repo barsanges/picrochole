f3f568a19a09253e6c351202dfed5b3253076779
barsanges <barsanges@users.noreply.github.com>
2022-09-20 22:00:33 +0200

    Teste la phase d'encerclement.

---
+++ test/Engine/Turn/Encirclement/Simple/TestSpec.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{- |
+   Module      : Engine.Turn.Encirclement.Simple.TestSpec
+   Copyright   : Copyright (C) 2022 barsanges
+   License     : GNU GPL, version 3
+-}
+
+module Engine.Turn.Encirclement.Simple.TestSpec ( spec ) where
+
+import Test.Hspec
+
+import Picrochole.Data.Units
+import qualified Picrochole.Data.Units as U
+import Picrochole.Engine.Turn ( encirclement )
+import Picrochole.JSON.Pieces
+
+instance Eq Unit where
+  x == y = x `approxEq` y
+
+spec :: Spec
+spec = do
+  it "units are destroyed if they are surrounded" $ do
+    mpieces <- loadSomeDir "test/Engine/Turn/Encirclement/Simple/"
+    mexpected <- readUnits "test/Engine/Turn/Encirclement/Simple/expected-units.json"
+    case (mpieces, mexpected) of
+      (Left m, _) -> expectationFailure ("unable to parse the test files, got: " ++ m)
+      (Right _, Left m) -> expectationFailure ("unable to parse the result file, got: " ++ m)
+      (Right pieces, Right expected) -> case (satlas pieces)
+                                             &> (sunits pieces)
+                                        of
+        Nothing -> expectationFailure "missing test files"
+        Just (atlas, units) -> do
+          let res = encirclement
+                    atlas
+                    units
+          (U.toList res) `shouldMatchList` (U.toList expected)
