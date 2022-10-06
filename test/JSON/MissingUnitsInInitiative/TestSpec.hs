96b21785081f2112ba46640cfef39ece6ffa1217
barsanges <barsanges@users.noreply.github.com>
2022-10-06 20:57:52 +0200

    Ajoute un test suite au commit e186ff2.

---
+++ test/JSON/MissingUnitsInInitiative/TestSpec.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+{- |
+   Module      : JSON.MissingUnitsInInitiative.TestSpec
+   Copyright   : Copyright (C) 2022 barsanges
+   License     : GNU GPL, version 3
+-}
+
+module JSON.MissingUnitsInInitiative.TestSpec ( spec ) where
+
+import Test.Hspec
+
+import Picrochole.JSON.Pieces
+
+spec :: Spec
+spec = do
+  it "fail to parse the files if all units are not listed in the initiative file" $ do
+    everything <- loadEverythingDir "test/JSON/MissingUnitsInInitiative"
+    case everything of
+      Left m -> m `shouldBe` "some units are not listed in the initiative file: [UK \"1st Red\"]"
+      Right _ -> expectationFailure "the files should fail to be parsed properly"
